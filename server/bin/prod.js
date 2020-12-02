985ce11cd0682dd6ea158a38272a1f7df188817e
trongnv <trongnv138@gmail.com>
2020-12-02 22:15:25 +0700

    auth docs

--- server/bin/prod.js
+++
@@ -1,8 +0,0 @@
-const errorHandle = require('../src/middleware/errorHandle');
-const routers = require('../src/routes');
-const app = require('../app');
-
-app.use(routers);
-app.use(errorHandle);
-
-module.exports = app;
